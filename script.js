7b8a82075d3d70f669d6d3cab7b7dc18528482a4
Glitch (puzzle-snagglefoot) <none>
2018-09-20 12:21:03 +0000

    ⚾️🎉 Checkpoint ./verbs.js:24237/11355 ./index.html:24237/885 ./script.js:24237/3962 ./style.css:24237/47

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var currentWordIndex = 0;
+var wasCorrect = false;
+shuffleArray(verbs);  
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+function start() {
+  showNextWord();
+}
+
+function showNextWord() {
+    if (currentWordIndex !== 0) {
+        addWordToResultTable(verbs[currentWordIndex]);
+    }
+    var currentWord = verbs[++currentWordIndex];
+    document.getElementById("ru_word").innerHTML = currentWord.ru.join(', ');
+}
+
+function addWordToResultTable(word) {
+    var table = document.getElementById("results");
+    var newRow = table.insertRow(1);
+    var ruCell = newRow.insertCell(0);
+    ruCell.innerHTML = word.ru.join(', ');
+    var enCell = newRow.insertCell(1);
+    enCell.innerHTML = word.en;
+    var correctnesCell = newRow.insertCell(2);
+    correctnesCell.innerHTML = wasCorrect ? '+' : '';
+}
+
+function know() {
+    wasCorrect = true;
+    showNextWord();
+}
+
+function dontKnow() {
+    wasCorrect = false;
+    showNextWord();
+}
+
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
